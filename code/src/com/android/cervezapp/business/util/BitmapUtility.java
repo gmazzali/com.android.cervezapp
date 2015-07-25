3fd9ff07aa877cd8f289cd5f0598d5d67b5085b1
Behe13 <Martin@FuegoAndante>
2015-07-25 11:08:04 -0300

    Mierda loco!

--- code/src/com/android/cervezapp/business/util/BitmapUtility.java
+++ code/src/com/android/cervezapp/business/util/BitmapUtility.java
@@ -24,7 +24,7 @@ public class BitmapUtility {
 	}
 
 	/*
-	 * Permite obtener una imagen a partir de un array de bytes.
+	 * Permite obtener una imagen a partir de un array de bytes.El choto!
 	 */
 	public static Bitmap getImage(byte[] image) {
 		return BitmapFactory.decodeByteArray(image, 0, image.length);
