86da8f8f60bf435b62c5640918083f82469e1575
Guillermo Sebastián Mazzali <guillemazzali@hotmail.com>
2015-07-25 13:49:53 -0300

    Carga de la enumeración de sexo para el usuario

---
+++ code/src/com/android/cervezapp/domain/util/SexoEnum.java
@@ -0,0 +1,26 @@
+package com.android.cervezapp.domain.util;
+
+import java.io.Serializable;
+
+/**
+ * @author Billy
+ */
+public enum SexoEnum implements Serializable {
+
+	MASCULINO("Masculino"), FEMENINO("Femenino");
+
+	private String sexo;
+
+	private SexoEnum(String sexo) {
+		this.sexo = sexo;
+	}
+
+	@Override
+	public String toString() {
+		return this.getSexo();
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+}
